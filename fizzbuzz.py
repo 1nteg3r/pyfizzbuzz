a4833ea4af9c921b243ba269c17488d1d318e9dd
1nteg3r <54937634+1nteg3r@users.noreply.github.com>
2022-06-01 00:22:05 +0200

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
-for i in range(1, 101):
-    fizzbuzz = i % 15 == 0
-    fizz = i % 5 == 0
-    buzz = i % 3 == 0
-    if fizzbuzz:
-        print(f"fizzbuzz {i} ")
-    elif fizz:
-        print(f"fizz {i}")
-    elif buzz:
-        print(f"buzz {i}"
+def main():
+    userinput = int(input("Enter a number range \n"))
+    for i in range(1,userinput+1):
+        if i%15 == 0:
+            print("Fizzbuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        elif i>0:
+            print(i)
+main()
+        
